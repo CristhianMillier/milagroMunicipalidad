366db93dd4ad73da2a9d609e4af7cd393d402e97
Cristhian Millier <millierflores123@gmail.com>
2025-03-21 20:01:50 -0500

    code simplificate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,9 +20,17 @@ class HomeController extends Controller
         if (Auth::check()){
             $usuario = User::find(Auth::user()->id);
             $roles = $usuario->getRoleNames();
-            $rol = $roles[0];
+
+            /**$rol = $roles[0];*/
+            $rol = $roles->first(); 
+
+            // Si el usuario no tiene rol, redirige o maneja el error
+            if (!$rol) {
+                Auth::logout();
+                return redirect()->route('login')->withErrors(['error' => 'Usuario sin rol asignado']);
+            }
             
-            $nusers = count(User::all());
+            /**$nusers = count(User::all());
             $ncargos = count(Cargo::all());
             $ncontratos = count(Contrato::all());
             $nlaborales = count(Regimene::all()->where('nombre','REG LAB'));
@@ -30,13 +38,30 @@ class HomeController extends Controller
             $npersonas = count(Persona::all());
             $ntipoplantillas = count(Tipo_plantilla::all());
             $nplantillas = count(Plantilla::all());
-            $nroles = count(Role::all());
+            $nroles = count(Role::all());**/
 
-            $query = Plantilla::select('mes', DB::raw('SUM(monto_total) as total_monto'))
+            $nusers = User::count();
+            $ncargos = Cargo::count();
+            $ncontratos = Contrato::count();
+            $nlaborales = Regimene::where('nombre', 'REG LAB')->count();
+            $nremuneraciones = Regimene::where('nombre', 'NIVEL REM')->count();
+            $npersonas = Persona::count();
+            $ntipoplantillas = Tipo_plantilla::count();
+            $nplantillas = Plantilla::count();
+            $nroles = Role::count();
+
+            /**$query = Plantilla::select('mes', DB::raw('SUM(monto_total) as total_monto'))
             ->where('anio', '=', DB::raw('CAST(YEAR(NOW()) AS CHAR)'))
             ->groupBy('mes', 'anio')
-            ->get();
+            ->get();*/
        
+
+            $query = Plantilla::select('mes', DB::raw('SUM(monto_total) as total_monto'))
+            ->where('anio', date('Y'))
+            ->groupBy('mes')
+            ->get();
+
+
             return view('dashboard.index',compact('nusers', 'ncargos', 'ncontratos', 'nlaborales', 'nremuneraciones', 'npersonas', 
                 'ntipoplantillas', 'nplantillas', 'nroles', 'rol', 'query'));
         } else{
